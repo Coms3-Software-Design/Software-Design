acb5881881307dca886cc27e8e80be71d6ce5b2c
Beasty786 <shameelnkosi@gmail.com>
2020-02-27 18:15:55 +0200

    fixing

--- app/src/main/java/com/example/marketplace/MainActivity.java
+++ app/src/main/java/com/example/marketplace/MainActivity.java
@@ -15,20 +15,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        final TextView t;
-        t = findViewById(R.id.textview1);
-        t.setOnClickListener(new View.OnClickListener() {
-            @SuppressLint("SetTextI18n")
-            @Override
-            public void onClick(View v) {
-                t.setAllCaps(true);
-                t.setText("Text has changed");
 
 
-                Toast.makeText(MainActivity.this,"Hey man you are here",Toast.LENGTH_LONG).show();
-                View l = findViewById(R.id.back);
-                l.setBackground(getDrawable(R.drawable.kali));
-            }
-        });
     }
 }
