560c136ca1f0cd694254bb1d457b5ce6ad96b985
Beasty786 <shameelnkosi@gmail.com>
2020-02-27 04:48:22 +0200

    this is trial just to see if it works fine

--- app/src/main/java/com/example/marketplace/MainActivity.java
+++ app/src/main/java/com/example/marketplace/MainActivity.java
@@ -3,6 +3,9 @@ package com.example.marketplace;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +13,16 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        final TextView t;
+        t = findViewById(R.id.textview1);
+        t.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                t.setAllCaps(true);
+                t.setText("Shameel");
+
+                Toast.makeText(MainActivity.this,"Hey man you are here",Toast.LENGTH_LONG).show();
+            }
+        });
     }
 }
