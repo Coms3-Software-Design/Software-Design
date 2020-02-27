5bd0da9356e7afa36eb1f574c68abe185e94e07a
Beasty786 <shameelnkosi@gmail.com>
2020-02-27 13:26:51 +0200

    fixing

--- app/src/main/java/com/example/marketplace/MainActivity.java
+++ app/src/main/java/com/example/marketplace/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.marketplace;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
 import android.os.Bundle;
 import android.text.Layout;
 import android.view.View;
@@ -17,10 +18,12 @@ public class MainActivity extends AppCompatActivity {
         final TextView t;
         t = findViewById(R.id.textview1);
         t.setOnClickListener(new View.OnClickListener() {
+            @SuppressLint("SetTextI18n")
             @Override
             public void onClick(View v) {
                 t.setAllCaps(true);
-                t.setText("Shameel");
+                t.setText("Text has changed");
+
 
                 Toast.makeText(MainActivity.this,"Hey man you are here",Toast.LENGTH_LONG).show();
                 View l = findViewById(R.id.back);
