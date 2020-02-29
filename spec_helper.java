a14e67b0b3885541f63ae3037133f193809b2f2b
Shameel <57645098+Beasty786@users.noreply.github.com>
2020-03-01 01:53:39 +0200

    Update spec_helper.java

--- spec_helper.java
+++ spec_helper.java
@@ -2,3 +2,32 @@ require 'coveralls'
 Coveralls.wear!
 
 # Your code here.
+  package com.example.marketplace;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.annotation.SuppressLint;
+import android.os.Bundle;
+import android.text.Layout;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        final TextView textView = (TextView) findViewById(R.id.textview1);
+        textView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                textView.setAllCaps(true);
+                textView.setText("Please Tell that thing to work");
+            }
+        });
+
+
+    }
+}
