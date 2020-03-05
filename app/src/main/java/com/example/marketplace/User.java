050c7dbf4eda3bf5ef6184a0745ebf23b23e3db0
Beasty786 <shameelnkosi@gmail.com>
2020-03-05 12:51:27 +0200

    added the user class

---
+++ app/src/main/java/com/example/marketplace/User.java
@@ -0,0 +1,127 @@
+package com.example.marketplace;
+
+public class User {
+
+    private String name, surname, userName, password, dateOfBirth, gender, dateCreated;
+    private StringBuilder bio;
+    private double balance;
+    private int contactDetails, userID;
+
+
+    /*
+    * Below is the User's constractor
+    * */
+    User(String name,String surname,String userName,String password,String dateOfBirth,String gender,String dateCreated,
+            StringBuilder bio , double balance , int contactDetails,int userID){
+
+        this.userID = userID;
+        this.name = name;
+        this.surname = surname;
+        this.userName = userName;
+        this.password = password;
+        this.dateOfBirth = dateOfBirth;
+        this.gender = gender;
+        this.dateCreated = dateCreated;
+        this.bio = bio;
+        this.balance = balance;
+        this.contactDetails = contactDetails;
+
+    }
+
+    /*
+    * Getters and setters below
+    */
+
+    public int getUserID() {
+        return userID;
+    }
+
+    public void setUserID(int userID) {
+        this.userID = userID;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getSurname() {
+        return surname;
+    }
+
+    public void setSurname(String surname) {
+        this.surname = surname;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getDateOfBirth() {
+        return dateOfBirth;
+    }
+
+    public void setDateOfBirth(String dateOfBirth) {
+        this.dateOfBirth = dateOfBirth;
+    }
+
+    public String getGender() {
+        return gender;
+    }
+
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
+
+    public String getDateCreated() {
+        return dateCreated;
+    }
+
+    public void setDateCreated(String dateCreated) {
+        this.dateCreated = dateCreated;
+    }
+
+    public StringBuilder getBio() {
+        return bio;
+    }
+
+    public void setBio(StringBuilder bio) {
+        this.bio = bio;
+    }
+
+    public double getBalance() {
+        return balance;
+    }
+
+    public void setBalance(double balance) {
+        this.balance = balance;
+    }
+
+    public int getContactDetails() {
+        return contactDetails;
+    }
+
+    public void setContactDetails(int contactDetails) {
+        this.contactDetails = contactDetails;
+    }
+
+    /*
+     * Getters and setters end here
+     */
+}
+
