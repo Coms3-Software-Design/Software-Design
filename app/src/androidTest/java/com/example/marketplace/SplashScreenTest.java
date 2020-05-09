66bd7cd24f9c5295f8e149a2972f5c503c688a55
Beasty786! <shameelnkosi@gmail.com>
2020-05-09 15:52:07 +0200

    Checking out as Beast, #NewBranch

---
+++ app/src/androidTest/java/com/example/marketplace/SplashScreenTest.java
@@ -0,0 +1,40 @@
+package com.example.marketplace;
+
+
+import android.view.View;
+import static org.junit.Assert.*;
+import androidx.test.ext.junit.runners.AndroidJUnit4;
+import androidx.test.rule.ActivityTestRule;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+@RunWith(AndroidJUnit4.class)
+public class SplashScreenTest{
+
+    @Rule
+    public ActivityTestRule<SplashScreen> splashScreenActivityTestRule = new ActivityTestRule<SplashScreen>(SplashScreen.class);
+
+    private SplashScreen sActivity = null;
+
+    @Before
+    public void setUp() throws Exception {
+        sActivity = splashScreenActivityTestRule.getActivity();
+    }
+
+    @Test
+    public void testlaunch(){
+        View view = sActivity.findViewById(R.id.loSplash);
+        View view1 = sActivity.findViewById(R.id.tvCopyRight);
+        assertNotNull(view);
+        assertNotNull(view1);
+    }
+
+    @After
+    public void tearDown() throws Exception {
+        sActivity = null;
+    }
+}
