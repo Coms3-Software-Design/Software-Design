570967064ac48434917ce229021c488812af71eb
Beasty786! <shameelnkosi@gmail.com>
2020-05-17 21:59:23 +0200

    UserInterface of Log in and the buy product viewing done

--- app/src/main/java/com/example/marketplace/SplashScreen.java
+++ app/src/main/java/com/example/marketplace/SplashScreen.java
@@ -4,17 +4,13 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.os.Handler;
 import android.view.View;
-import android.view.animation.Animation;
-import android.view.animation.AnimationUtils;
 import android.widget.Button;
 import android.widget.TextView;
 
 public class SplashScreen extends AppCompatActivity {
     TextView Register;
     Button Login;
-    Animation frombottom,fromtop,fromside;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -23,20 +19,11 @@ public class SplashScreen extends AppCompatActivity {
         Register = findViewById(R.id.tvSplashSignup);
         Login = findViewById(R.id.btnSplashLogin);
 
-        frombottom = AnimationUtils.loadAnimation(this,R.anim.frombottom);
-        fromtop = AnimationUtils.loadAnimation(this,R.anim.fromtop);
-        fromside = AnimationUtils.loadAnimation(this,R.anim.fromside);
-
-        fromtop.setDuration(3000);
-        frombottom.setDuration(2000);
-        fromside.setDuration(2500);
-
         Login.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Intent login = new Intent(SplashScreen.this, Login.class);
                 startActivity(login);
-                finish();
             }
         });
 
