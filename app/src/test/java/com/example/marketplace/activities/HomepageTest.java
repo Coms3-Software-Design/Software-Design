6fd4b459f42c4a194281345d7e032db7185fdcdb
Beasty786! <shameelnkosi@gmail.com>
2020-06-07 12:43:33 +0200

    updating registrations tests

--- app/src/test/java/com/example/marketplace/activities/HomepageTest.java
+++ app/src/test/java/com/example/marketplace/activities/HomepageTest.java
@@ -1,15 +1,34 @@
 package com.example.marketplace.activities;
 
+import android.content.Intent;
+
+import com.example.marketplace.classes.User;
+
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.robolectric.Robolectric;
+import org.robolectric.RobolectricTestRunner;
+import org.robolectric.RuntimeEnvironment;
 
 import static org.junit.Assert.*;
 
+@RunWith(RobolectricTestRunner.class)
+
 public class HomepageTest {
+
+    Homepage home;
+    User user;
 
     @Before
     public void setUp() throws Exception {
+        user = new User("1814732","s","d","c","P","0","27 aug","today","male","Hey man",25,"http://lamp.ms.wits.ac.za/~s1814731/MPphpfiles/uploads/");
+        Intent intent = new Intent(RuntimeEnvironment.systemContext , Homepage.class);
+        intent.putExtra("user",user);
+        home = Robolectric.buildActivity(Homepage.class,intent)
+                .setup()
+                .get();
     }
 
     @After
@@ -17,26 +36,10 @@ public class HomepageTest {
     }
 
     @Test
-    public void onNavigationItemSelected() {
+    public void check(){
+        assertNotNull(home);
     }
 
-    @Test
-    public void setUser() {
-    }
 
-    @Test
-    public void editProfile() {
-    }
 
-    @Test
-    public void initialise() {
-    }
-
-    @Test
-    public void onBackPressed() {
-    }
-
-    @Test
-    public void onPostResume() {
-    }
 }
